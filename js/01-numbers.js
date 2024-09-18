bf2ff0d2d14818efeb9f00edda5b0b6b9c46cdbc
Iryna <irasokolova25@ukr.net>
2024-09-18 19:37:51 +0300

    розібрали тему

--- js/01-numbers.js
+++ js/01-numbers.js
@@ -2,25 +2,39 @@
  *  Математичні оператори
  */
 
+let total = 0;
+
+const number1 = 5;
+const number2 = 15;
+
+total = number1 + number2;
+// console.log(total);
+
+total = number1 - number2;
+// console.log(total);
+
 /*
  * Парс числа з Number.parseInt() та Number.parseFloat()
  */
 
-const elementWidth = '50px';
 
-const parsedElementWidth = Number.parseInt(elementWidth);
-// console.log(parsedElementWidth);
 
-const elementHeight = '200.74px';
-const result = Number.parseFloat(elementHeight);
-// console.log(result);
+const imgWidth = '100.52px'
+const imgWidthParsed = Number.parseInt(imgWidth)
+console.log(imgWidthParsed);
 
+const borderLength = '15.245cm'
+const borderLengthParsed = Number.parseFloat(borderLength)
+console.log(borderLengthParsed);
+NaN
 /*
  * - Метод число.toFixed(digits)
  * - Матрьошки на пркладі console.log(Number(число.toFixed(digits)))
  */
 const salary = 1300.16472;
-// console.log(salary);
+const fixedSalary = salary.toFixed(0)
+
+console.log(Number(fixedSalary));
  
 // console.log(Number(salary.toFixed(1)));
 
@@ -66,7 +80,7 @@ const minNumber = Math.pow(base, power);
  * - Math.round()
  */
 const randomNumber = Math.round(Math.random() * (50 - 15) + 15)
-console.log(randomNumber);
+// console.log(randomNumber);
 
 
 const colors = ['tomato', 'teal', 'orange', 'deeppink', 'skyblue'];
@@ -77,6 +91,18 @@ const index = Math.round(Math.random() * (max - min) + min);
 
 const color = colors[index];
 
-console.log(color);
+// console.log(color);
 
 document.body.style.backgroundColor = color;
+
+
+
+/**
+ * Напишіть скрипт, який просить відвідувача ввести два числа, і в результаті показує їх суму.
+ */
+
+/**
+ * 1. створити змінні для збереження результатів промпту
+ * 2. перетворити результат промпту в число
+ * 3. додати чисто та вивести в консоль
+ */
--- js/02-comparison-operators.js
+++ js/02-comparison-operators.js
@@ -3,14 +3,13 @@
  * Оператори порівняння
  */
 
-console.log(10 % 4);//2
-10 % 5 //0
-15 % 6 //3
-23 % 12 //11
-33 % 14 //5
-
-
-
+const result = 5 > 1;
+// console.log(result);
+// console.log(10 % 4);//2
+// 10 % 5 //0
+// 15 % 6 //3
+// 23 % 12 //11
+// 33 % 14 //5
 
 // console.log(15 > 3);
 
@@ -20,30 +19,30 @@ console.log(10 % 4);//2
 
 // console.log('10' > 5);
 
+
+console.log(100 > 100);//false
+console.log(100 >= 100);//true
+console.log(50 < 20);//false
+console.log(2 <= 4);//true
+console.log('bob' !== 'bob');//false
 /*
  * Оператори рівності (== и ===) та нерівності (!= и !==)
  * ✅ «ЗАВЖДИ ВИКОРИСТОВУЙТЕ СУВОРУ РІВНІСТЬ (===) ТА НЕРІВНІСТЬ (!==)»
  */
-const num = 20;
-const str = '20';
+// '5' == 5//true
 
-10 == 10 //true
-10 === 10 //true
 
-20 == '20' //true
-num === str //false
+console.log(5 !== 10);//true
+console.log();
+// ! - не
 
-'' == false //true
-'' === false //false
 
-!false //true
-!true //false
 /**
  * 4. Що буде в консолі
-            const isEqual = 0 == false; //true
+            const isEqual = 0 === false; 
             const isEqual = 0 !== false; 
-            const isEqual = '12' == 12; //true
-            const isEqual = '12' === 12; //false
+            const isEqual = '12' === 12; 
+            const isEqual = '12' === 12; 
  */
 
 
