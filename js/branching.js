544b162649e39a2f27b7b587be31061efc3e5c1c
Iryna <irasokolova25@ukr.net>
2024-10-02 19:37:08 +0300

    п

--- js/branching.js
+++ js/branching.js
@@ -2,20 +2,52 @@
  * Оператор розгалуження if
  */
 
+// console.log('Код до if');
+
+// if (true) {
+//     console.log('Код в середині if');
+// } else {
+    
+// }
+
+// console.log('Код після if');
+
 /*
  * Оператор розгалуження if...else
  */
 
 let totalPrice;
-const quantity = 6;
+const quantity = 7;
 const price = 150;
 
+// totalPrice = quantity * price;
+// console.log(totalPrice);
+
+// if (totalPrice <= 1000) {
+//     console.log('Дякую за замовлення!');
+// } else {
+//     console.log('Ваш ліміт обмежено!');
+// }
+
+// console.log('Дякую, що завітали на наш сайт');
 
 /*
  * Оператор розгалуження else...if
  */
 
-const salary = 3500;
+const salary = 4500;
+
+if (salary <= 1000) {
+    console.log('1 рівень досвіду');
+} else if (salary > 1000 && salary <= 2000) {
+    console.log('2 рівень досвіду');
+} else if (salary > 2000 && salary <= 3000) {
+    console.log('2 рівень досвіду');
+} else {
+    console.log('Керівник відділу');
+}
+
+console.log('Код за else if');
 
 /*
  * Тернарний оператор
--- js/logical-operators.js
+++ js/logical-operators.js
@@ -2,17 +2,54 @@
  *  Приведення до булю на прикладі Boolean(value)
  */
 
+//true false
+
+/**
+ * FALSE
+ * 1.false
+ * 2.null
+ * 3.undefined
+ * 4.NaN
+ * 5.0
+ * 6.'' або ""
+ */
+
+// console.log(Boolean(5));
+
+
 /*
  * Логічне І (оператор &&)
  * - Запинається на брехні
  * - Повертає те на чому запнулось або останній операнд
  */
+                //  true      false  true
+// const andResult =  'message' && 0 && 10
+// console.log(andResult);
+
+// const quantity = prompt('Введіть кількість товару')
+// const message = quantity && 'Дякую за замовлення'
+
+// console.log(message);
+
+'' && 10 && 0 // ''
             
 /*
  * Логічне АБО (оператор ||)
  * - Запинаєтьс на правді
  * - Повертає те на чому запнулось або останній операнд
  */
+
+const orResuld = 0 || null || 'Hello' || 5
+
+console.log(orResuld);
+
+10 || undefined || 'kjbfjfh' // 10
+
+// true - true
+// !true - false
+
+!0 //true
+!'hello' //false
 
 /*
  * Логічне НІ (оператор !)
