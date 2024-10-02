b743e0b5bee1d6e294259d87374c7aab289ca430
Iryna <irasokolova25@ukr.net>
2024-10-02 15:47:05 +0300

    add files

---
+++ js/branching.js
@@ -0,0 +1,26 @@
+/*
+ * Оператор розгалуження if
+ */
+
+/*
+ * Оператор розгалуження if...else
+ */
+
+let totalPrice;
+const quantity = 6;
+const price = 150;
+
+
+/*
+ * Оператор розгалуження else...if
+ */
+
+const salary = 3500;
+
+/*
+ * Тернарний оператор
+ */
+
+// const salary = 5000;
+// let message;
+
