6653b3bd46a8afdce5d08c74e0c99f5cc1c20735
Iryna <irasokolova25@ukr.net>
2024-12-10 11:15:34 +0200

    Update 01-basics.js

--- js/01-basics.js
+++ js/01-basics.js
@@ -11,13 +11,13 @@
 const arr = []
 const names = ['Mark', 'Andrjy', 'Demian', 'Danya'];
 
-console.log(names);
+// console.log(names);
 // console.log(names.length);
 // console.log(names[2]);
 
 names[1] = 'Andriy'
 
-console.log(names);
+// console.log(names);
 
 /*
  * Передача по посиланню і по значенню
@@ -32,6 +32,24 @@ console.log(names);
  * - for - якщо потрібен індекс або потрібно змінити елемент масиву
  * - for...of - якщо індекс не потрібен і в масиві нічого змінювати не потрібно
  */
+
+const cities = ['Kharkiv', 'Lviv', 'Kyiv', 'Dnipro', 'Poltava', 'Zhytomyr']
+
+for (let city of cities) {
+    // console.log(city);
+
+    city += '!'
+}
+
+// for (let i = 0; i <= cities.length - 1; i += 1){
+//     console.log(i);
+//     console.log(cities[i]);
+//     cities[i]+='!'
+// }
+
+console.log(cities);
+
+
 
 // const names = ['Ola', 'Petra', 'Nastia', 'Mary'];
 
