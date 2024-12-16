0c99ad7942d601fbfa55af00e50eb016dbe9919c
Iryna <irasokolova25@ukr.net>
2024-12-16 15:20:12 +0200

    add plan

--- index.js
+++ index.js
@@ -1,51 +1,96 @@
-const name = 'Bob';
-const age = 15;
-
-console.log(name);
-console.log('Hello');
-
-let message;
-
-// message = 'Привіт, студент!';
-
-console.log(message);
-
-// Примітивні типи данних
-
-// String - рядок
-const user = 'Nikita';
-const userLength = user.length
-console.log(user.toLocaleUpperCase());
-console.log(userLength);
-
-
-// Number - число
-const quantity = 10;
-
-// Undefined
-// Null
-let userData = null;
-
-// Boolean - буль
-const isHidden = false;
-const isOnline = true;
-const shouldOpen = false;
-
-// Взаємодія з користувачем
-
-/**
- * ALERT
+/*
+ * Функції
+ * - Функціональний вираз (function expression)
+ * - Оголошення функції (function declaration)
+ * - Аргументи і параметри
+ * - return
+ * - Поверненнф значення
+ * - Параметри за замовчуванням
  */
-// alert('Привіт! Гарного настрою))')
 
-/**
- * CONFIRM
- */
-// const userAnswer = confirm('Ви впевнені, що хочете покинути сторінку?');
-// console.log("userAnswer: ", userAnswer);
+// const fnA = function(param1){};
 
-/**
- * PROMPT
- */
-const promptResult =  prompt('Підпишіться на розсилку спаму')
-console.log('promptResult: ', promptResult);
+// const fnB = param1 => param1.toLowerCase();
+
+// console.log(fnB('Hello'));
+
+const a = function() {};
+const b = a;
+
+console.log(a === b);
+
+// const addPrice = function(a, b){  
+//   // console.log('a: ', a);
+//   // console.log('b: ', b);
+
+//  return a + b;  
+// }
+
+// const res1 = addPrice(5, 15);
+// console.log(res1);
+
+// addPrice(15, 20);
+// addPrice(20, 15);
+// addPrice(10, 25);
+
+  
+  
+  // Function declaration
+  // findLogin('login');
+ 
+  // function findLogin(login, arr) {
+  //   console.log(login);
+  // }
+
+  // findLogin('login');
+
+  /*
+   * - Стек викликів
+   * - Stack trace і пошук помилок
+   */
+  
+  // const fnA = function() {
+  //   console.log('Почала виконуватися [fnA]');
+  //   fnB();
+  //   console.log('Продовжила виконуватися [fnA] після виходу з [fnB]');
+  // };
+  
+  // const fnB = function() {
+  //   console.log('Виконується [fnB]');
+  // };
+  
+  // console.log('Почав виконання [main]');
+  // fnA();
+  // console.log('Продовжив виконуватися [main] після виходу з [fnA]');
+    
+    
+    
+    
+  
+  
+  
+  
+  
+  // Створіть масив styles з елементами 'Джаз' i 'Блюз'. 
+  // Додайте 'Рок-н-ролл' в кінець масиву. 
+  // Замініть значення в середині масиву на 'Класика'. 
+  // Видаліть перший елемент масиву і покажіть його. 
+  // Додайте 'Реп' і 'Реггі' на початок масиву. 
+  // Виведіть масив в консоль.
+
+
+
+  // ЗАДАЧА №5 ДЗ5
+  // const styles = ['Джаз', 'Блюз'];
+  // styles.push('Рок-н-ролл');
+
+  // console.log(styles);
+
+  // styles.splice(1, 1, 'Класика');
+  // console.log(styles);
+
+  // styles.shift();
+  // console.log(styles);
+
+  // styles.unshift('Реп','Реггі')
+  // console.log(styles);
