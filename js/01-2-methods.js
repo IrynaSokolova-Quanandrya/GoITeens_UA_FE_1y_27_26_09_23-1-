32e9421f1fdbd1b641dc1dcfde6f45f8a03157ec
Iryna <irasokolova25@ukr.net>
2024-12-25 19:36:49 +0200

    add

---
+++ js/01-2-methods.js
@@ -0,0 +1,23 @@
+/*
+ * Методи об"екту і this при звернені до властивостей в методах
+ *
+ * http://fecore.net.ua/books/m5ph3r-javascript/module-03/images/context.jpg
+ *
+ * - changeName
+ * - addTrack
+ * - updateRating
+ * - getTrackCount
+ */
+const playlist = {
+      name: 'Плейлист',
+      rating: 5,
+      tracks: ['трек-1', 'трек-2', 'трек-3'],
+
+}
+
+console.log(playlist);
+
+
+
+
+
