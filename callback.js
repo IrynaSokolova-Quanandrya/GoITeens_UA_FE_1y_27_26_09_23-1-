17d21c3e22ad611de0ffc0ab51cac24f8eb3c433
Iryna <irasokolova25@ukr.net>
2024-12-26 18:26:39 +0200

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const getUserName = function(userName) {
     console.log(userName);
 }
@@ -9,3 +10,12 @@ const logUserName = function (callback) {
 }
 
 logUserName(getUserName)
+=======
+/**
+ * Функція вищого порядку  — функція, що приймає в якості параметрів інші
+ * функції або повертає функцію як результат.
+ * 
+ * Функція зворотного виклику (callback)  — це функція, яка передається іншій
+ * функції як аргумент і та, в свою чергу, викликає передану функцію.
+ */
+>>>>>>> Stashed changes
