ad4b68863b95b6321989f58cdd07d0a69eb8f614
Iryna <irasokolova25@ukr.net>
2025-01-07 14:49:01 +0200

    Update callback.js

--- callback.js
+++ callback.js
@@ -1,4 +1,11 @@
-<<<<<<< Updated upstream
+/**
+ * Функція вищого порядку  — функція, що приймає в якості параметрів інші
+ * функції або повертає функцію як результат.
+ * 
+ * Функція зворотного виклику (callback)  — це функція, яка передається іншій
+ * функції як аргумент і та, в свою чергу, викликає передану функцію.
+ */
+
 const getUserName = function(userName) {
     console.log(userName);
 }
@@ -10,12 +17,27 @@ const logUserName = function (callback) {
 }
 
 logUserName(getUserName)
-=======
+
 /**
- * Функція вищого порядку  — функція, що приймає в якості параметрів інші
- * функції або повертає функцію як результат.
- * 
- * Функція зворотного виклику (callback)  — це функція, яка передається іншій
- * функції як аргумент і та, в свою чергу, викликає передану функцію.
+ * Напишіть функцію doTask(callback), яка викликає передану їй функцію.
+ * Напишіть функцію modifyArray(arr, callback), яка приймає масив і колбек для 
+ * зміни кожного елемента масиву (збільшення кожного елемента вдвічі).
  */
->>>>>>> Stashed changes
+
+
+function modifyArray(array, callback) {
+    let newArray = [];
+    for (const element of array) {
+        if (callback(element)) {
+            newArray.push(element);
+        }
+    }
+
+    return newArray
+}
+
+function callback1(number) {
+   return number >= 3
+}
+
+modifyArray([1, 2, 3, 4, 5], callback1); // [3, 4, 5]
