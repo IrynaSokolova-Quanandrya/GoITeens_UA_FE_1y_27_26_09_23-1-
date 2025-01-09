39123d8547779741ebbd744ec33506f41a41ac56
Iryna <irasokolova25@ukr.net>
2025-01-09 23:10:33 +0200

    add

--- js/01-2-methods.js
+++ js/01-2-methods.js
@@ -12,9 +12,12 @@ const playlist = {
       name: 'Плейлист',
       rating: 5,
       tracks: ['трек-1', 'трек-2', 'трек-3'],
-
+      changeName(newName) {
+            this.name = newName
+      }
 }
 
+playlist.changeName('My playlist')
 console.log(playlist);
 
 
--- js/01-3-iteration.js
+++ js/01-3-iteration.js
@@ -7,62 +7,13 @@ const feedback = {
     neutral: 10,
     bad: 3,
   };
-  // Number Array Boolean Object
-
-  const allKeys = Object.keys(feedback);
-  console.log(allKeys);
-
-// feedback.good
-// feedback['good'];
-// feedback['neutral'];
-// feedback['bad'];
-
-let total = 0;
-  // for (const key of allKeys) {
-  //   // console.log(feedback[key]);
-  //   total += feedback[key]
-  // }
-
-// console.log(total);
-
-// const values = Object.values(feedback);
-// console.log(values);
-
-// for (const value of values) {
-//   console.log(value);
-//   total += value;
-// }
-
-// const entries = Object.entries(feedback)
-// console.log(entries);
-
-  // Number Array Boolean Object
-
-  // const keys = Object.keys(feedback);
-  // console.log(keys);
-
-  // let totalFeedback = 0;
-
-  // for (const key of keys) {
-  //   // console.log(feedback[key]);
-
-  //   totalFeedback += feedback[key]
-  // }
-
-  // console.log('totalFeedback: ', totalFeedback);
-
-// const values = Object.values(feedback);
-//  console.log(values);
-
-//  let totalFeedback = 0;
-
-//   for (const value of values) {
-//     console.log(value);
-
-//     totalFeedback +=  value
-//    }
-//    console.log('totalFeedback: ', totalFeedback);
-
-  //  const arr = Object.entries(feedback);
-
-  //  console.log(arr);
+  
+  /**
+   * hasOwnProperty('property') - повертає true, якщо властивість міститься 
+   * лише у вказаному об'єкті.
+   * оператор in - повертає true, якщо властивість міститься у вказаному   
+   * об'єкті або його ланцюжку прототипів. ('property' in object)
+   */
+const user = {
+  score: 0
+}
