49cec4977fe21094ee18a1bafd8af6c4059968ea
Iryna <irasokolova25@ukr.net>
2025-01-28 18:17:57 +0200

    add

---
+++ js/01-foreach.js
@@ -0,0 +1,13 @@
+/*
+ * Array.prototype.forEach(callback(currentValue, index, array), thisArg)
+ * - Поелементо перебирає оригінальний масив
+ * - Нічого не повертає
+ * - Замінює класичний for, якщо не потрібно преривати цикл
+ */
+
+// array.method(callback[currentValue, index, array])
+
+const numbers = [5, 10, 15, 20, 25];
+
+
+console.log(numbers)
