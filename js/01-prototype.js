a97b8a3f72aa1d02a8037b6d4bad6b6fa8969ddb
Iryna <irasokolova25@ukr.net>
2025-02-20 19:00:03 +0200

    прототип

--- js/01-prototype.js
+++ js/01-prototype.js
@@ -4,3 +4,26 @@
 * [[Prototype]]
 * Ланцюг прототипів
 */
+
+const user = {
+    name: 'Amy',
+    age: 15,
+
+    changeName(newName) {
+        this.name = newName
+    }
+}
+
+user.changeName('Mary')
+console.log(user);
+
+
+// Array
+// Boolean
+// Object
+
+const names = ['Bob', 'John']
+
+console.log(names);
+// names.map()
+
--- js/02-class.js
+++ js/02-class.js
@@ -1,3 +1,36 @@
 /**
  * Основи ООП: клас, об'єкт, інтерфейс
  */
+
+/*
+ * Класи
+ * 🐷 - об'явлення (Car)
+ * 🐷 - конструктор
+ * 🐷 - методи 
+ * 🐷 - static(description, logInfo)
+ * 🐷 - приватні властивості
+ * 🐷 - синтаксис публічних властивостей та методи класів
+ * 🐷 - Геттери та сеттери
+ */
+
+let total = 0;
+const num1 = 5;
+const num2 = 10;
+
+function getSum(a, b) {
+    return total = a + b    
+}
+getSum(num1, num2)
+
+
+const add = {
+    total: 0,
+    num1: 5,
+    num2: 10,
+    
+    getSum() {
+    return this.total = this.num1 + this.num2    
+}
+}
+
+add.getSum()
