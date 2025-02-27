a8c90e3408698b0bfc32f93413c31fc4c3f4fc22
Iryna <irasokolova25@ukr.net>
2025-02-27 19:58:24 +0200

    фвв

--- js/02-class.js
+++ js/02-class.js
@@ -17,17 +17,33 @@ class Car {
     static description = 'Опис автомобіля'
     #region = 5
     constructor({model, price, color}) {
-        this.model = model
+        this._model = model
         this.price = price
-        this.color = color
-       
-}
+        this.color = color    
+    }
+    
+    get model() {
+        console.log(this._model);
+    }
+
+    set model(newModel) {
+        this._model = newModel
+    }
     }   
+    
     
 
 
 
-const car1 = new Car({model:'Audi',price:50000,color:'red' })
+const car1 = new Car({ model: 'Audi', price: 50000, color: 'red' })
+
+car1.getModel()
+car1.setModel('Audi Q6')
+
+// геттери та сеттери
+car1.model
+car1.model = 'Audi Q6'
+
 const car2 = new Car({model:'Mercedec',price:90000,color:'white' })
 const car3 = new Car({model:'Mazda',price:5000,color:'black' })
 
--- js/03-inheritance.js
+++ js/03-inheritance.js
@@ -15,19 +15,34 @@
  * cast
  */
 class Hero {
+    constructor({name, xp}) {
+        this.name = name
+        this.xp = xp
+    }
+
 }
 
-class Warrior  {
+class Warrior extends Hero {
+    constructor({ weapon }) {
+        super()
+       this.weapon = weapon
+    }
+    
 }
+const mango = new Warrior({ name: 'mango', xp: 1000, weapon: 'алебарда' });
 
-class Mage {
+class Mage extends Hero{
+    constructor({ spells }) {
+        super()
+        this.spells = spells
+    }
 
     cast() {
         console.log(`${this.name} щось там чаклує 🧙‍♂️`);
     }
 }
+const poly = new Mage({ name: 'poly', xp: 500, spells: ['фаербол'] });
 
-// const mango = new Warrior({ name: 'mango', xp: 1000, weapon: 'алебарда' });
 // console.log(mango);
 // mango.attack();
 // mango.gainXp(1000);
