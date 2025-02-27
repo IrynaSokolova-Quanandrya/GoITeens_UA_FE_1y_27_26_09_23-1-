6556d5257ffbdc18f42444b7ac46e43d1084f54d
Iryna <irasokolova25@ukr.net>
2025-02-27 18:34:49 +0200

    finished

--- js/02-class.js
+++ js/02-class.js
@@ -13,24 +13,24 @@
  * 🐷 - Геттери та сеттери
  */
 
-let total = 0;
-const num1 = 5;
-const num2 = 10;
-
-function getSum(a, b) {
-    return total = a + b    
+class Car {
+    static description = 'Опис автомобіля'
+    #region = 5
+    constructor({model, price, color}) {
+        this.model = model
+        this.price = price
+        this.color = color
+       
 }
-getSum(num1, num2)
-
-
-const add = {
-    total: 0,
-    num1: 5,
-    num2: 10,
+    }   
     
-    getSum() {
-    return this.total = this.num1 + this.num2    
-}
-}
 
-add.getSum()
+
+
+const car1 = new Car({model:'Audi',price:50000,color:'red' })
+const car2 = new Car({model:'Mercedec',price:90000,color:'white' })
+const car3 = new Car({model:'Mazda',price:5000,color:'black' })
+
+console.log(car1);
+console.log(car2);
+console.log(car3);
