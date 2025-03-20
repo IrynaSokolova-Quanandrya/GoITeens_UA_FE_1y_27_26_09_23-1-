19a49d4f931d917b01c81f335b151285be92a742
Iryna <irasokolova25@ukr.net>
2025-03-20 22:45:27 +0200

    Update 01-event-listeners.js

--- js/01-event-listeners.js
+++ js/01-event-listeners.js
@@ -13,16 +13,18 @@ const targetBtn = document.querySelector('.js-target-btn');
 const addListenerBtn = document.querySelector('.js-add-listener');
 const removeListenerBtn = document.querySelector('.js-remove-listener');
 
-addListenerBtn.addEventListener('click', () => {
-         console.log('Вішаємо');
-        targetBtn.addEventListener('click', handleClick)
-})
+// addListenerBtn.addEventListener('click', () => {
+//          console.log('Вішаємо');
+//         targetBtn.addEventListener('click', handleClick)
+// })
 
-removeListenerBtn.addEventListener('click', () => {
-         console.log('Знімаємо');
-       targetBtn.removeEventListener('click', handleClick) 
-})
+// removeListenerBtn.addEventListener('click', () => {
+//          console.log('Знімаємо');
+//        targetBtn.removeEventListener('click', handleClick) 
+// })
 
-function handleClick() {
-          console.log('Додали слухача targetBtn');
+targetBtn.addEventListener('click', handleClick)
+
+function handleClick(event) {
+          console.log(event.currentTarget === targetBtn);
 }
