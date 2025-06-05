12d0be80e5f358d816680fd6e4d5c0d2f259c8b3
Iryna <irasokolova25@ukr.net>
2025-06-05 20:34:41 +0300

    done

---
+++ vite-project/src/filteredNumbers.js
@@ -0,0 +1,9 @@
+// іменний експорт
+export const filterNumbers = (numbers) => {
+    const filterResult = numbers.filter(number => number >= 10)
+    console.log(filterResult);
+}
+
+export const user = 'Bob'
+export const num = [10, 15, 30]
+
---
+++ vite-project/src/main.js
@@ -0,0 +1,5 @@
+// іменний імпорт
+import { filterNumbers } from "./filteredNumbers";
+
+
+filterNumbers([10, 5, 18, 12, 6, 1])
