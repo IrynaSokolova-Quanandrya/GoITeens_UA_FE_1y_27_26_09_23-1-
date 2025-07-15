95f3c2cb7632c3b4be5332ca76fcb3695817e9ce
Iryna <irasokolova25@ukr.net>
2025-07-15 21:49:18 +0300

    add files

--- index.js
+++ index.js
@@ -1,51 +1,14 @@
-const name = 'Bob';
-const age = 15;
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target); // відключити спостереження після першої появи
+    }
+  });
+}, {
+  threshold: 0.1 // спрацює, коли видно хоча б 10% елемента
+});
 
-console.log(name);
-console.log('Hello');
-
-let message;
-
-// message = 'Привіт, студент!';
-
-console.log(message);
-
-// Примітивні типи данних
-
-// String - рядок
-const user = 'Nikita';
-const userLength = user.length
-console.log(user.toLocaleUpperCase());
-console.log(userLength);
-
-
-// Number - число
-const quantity = 10;
-
-// Undefined
-// Null
-let userData = null;
-
-// Boolean - буль
-const isHidden = false;
-const isOnline = true;
-const shouldOpen = false;
-
-// Взаємодія з користувачем
-
-/**
- * ALERT
- */
-// alert('Привіт! Гарного настрою))')
-
-/**
- * CONFIRM
- */
-// const userAnswer = confirm('Ви впевнені, що хочете покинути сторінку?');
-// console.log("userAnswer: ", userAnswer);
-
-/**
- * PROMPT
- */
-const promptResult =  prompt('Підпишіться на розсилку спаму')
-console.log('promptResult: ', promptResult);
+document.querySelectorAll('.section').forEach(section => {
+  observer.observe(section);
+});
